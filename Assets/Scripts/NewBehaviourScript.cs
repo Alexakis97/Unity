520bb241366727782303e1d984193cf87ba9e371
Alexakis97 <filspd2@hotmail.com>
2020-10-17 17:14:26 -0700

    Final Commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+  public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        print("My First Script");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        rb.AddForce(0,0,-20);
     }
 }
